af31555467a52745e102631ac2a41616fb9b6949
wuliji <957090687@qq.com>
2019-05-07 17:24:14 +0800

    git测试

---
+++ src/main/java/com/wuliji/seckill/Test.java
@@ -0,0 +1,13 @@
+package com.wuliji.seckill;
+
+/**
+ * @Auther: wanglunjie
+ * @Date: 2019/5/7 17:20
+ * @Description:
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
